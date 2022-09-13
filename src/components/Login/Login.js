b292627111cd3beae738199d60d51cad1fc20f86
ahbab1995 <islamahbab1996@yahoo.com>
2022-09-13 18:02:02 +0600

    require auth new code added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import "./Login.css";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [setError] = useState("");
 
   const navigate = useNavigate();
+
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
 
   const [signInWithEmailAndPassword, user, loading, error] =
     useSignInWithEmailAndPassword(auth);
@@ -28,7 +30,7 @@ const Login = () => {
   };
 
   if (user) {
-    navigate("/shop");
+    navigate(from,{replace : true});
   }
 
   return (
